871b3490dce1991cc3d7997b9829c0479b8681a3
Arthur <65630024+Arthur-nonaka@users.noreply.github.com>
2024-08-16 17:26:24 -0300

    Add files via upload

--- Fila/Program.cs
+++ Fila/Program.cs
@@ -4,11 +4,20 @@ namespace filas{
     class Program {
         static void Main(string[] args) {
             Fila fila = new Fila();
+
             fila.insert(10);
+            fila.insert(-20);
+            fila.insert(-30);
+            fila.insert(-50);
             fila.insert(20);
-            fila.insert(30);
-            fila.remove();
-            fila.show();
+
+            Fila fila2 = new Fila();
+            fila2.insert(1436430);
+            fila2.insert(-643646);
+            fila2.insert(54350);
+
+            Fila fila3 = fila.Join(fila2);
+            fila3.show();
 
         }
     }
--- Fila/obj/Debug/net8.0/Fila.AssemblyInfo.cs
+++ Fila/obj/Debug/net8.0/Fila.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Fila")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a41b8eb411e9020c1004085052eb89a599cb269e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdf33d61cf21a64e7641a275b5cee625ff094708")]
 [assembly: System.Reflection.AssemblyProductAttribute("Fila")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Fila")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
